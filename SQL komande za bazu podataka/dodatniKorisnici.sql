a1b164340d200bcf24139cf4ae213dd994c818f8
TojzanKristian <116062572+TojzanKristian@users.noreply.github.com>
2023-01-10 00:02:18 +0100

    SQL

---
+++ SQL komande za bazu podataka/dodatniKorisnici.sql
@@ -0,0 +1,29 @@
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Nikola Aleksic','Partizanska',0,27,'Novi Sad','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Valentina Vasiljevic','Bogdana Suputa',0,33,'Vrbas','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Nevena Nikic','Cirila i Metodija',0,11,'Becej','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Nemanja Bojic','Nikole Tesle',0,5,'Jagodina','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Nikoleta Tot','Napredna',0,97,'Subotica','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Mario Mikic','Bulevar Cara Lazara',0,13,'Beograd','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Ana Markovic','Petra Bigina',0,76,'Novi Pazar','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Rebeka Ric','Jovana Jovanovica Zmaja',0,46,'Srbobran','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Milos Marinkovic','Vuka Karadica',0,24,'Feketic','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Helena Hrnjak','Jovana Cvijica',0,44,'Nis','Srbija');
