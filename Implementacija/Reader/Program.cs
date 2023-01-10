1d617e95cb085a9984031a225dc107d3d2831c81
TojzanKristian <116062572+TojzanKristian@users.noreply.github.com>
2023-01-10 11:20:06 +0100

    Klasa Reader

---
+++ Implementacija/Reader/Program.cs
@@ -0,0 +1,79 @@
+﻿using CacheMemory.Reader;
+using CacheMemory.Structures.Implementations;
+
+Historical historical = new();
+
+Reader reader = new Reader(historical);
+
+//reader.SearchByMonth(1);
+
+var option = string.Empty;
+while (!option.Equals("0"))
+{
+    Console.WriteLine("----------------------------------------------------------------");
+    Console.WriteLine("Unesite opciju po kojoj želite da se ispišu podaci :");
+    Console.WriteLine("\t1. Ispis podataka po mesecu!");
+    Console.WriteLine("\t2. Ispis podataka po IDu korisnika!");
+    Console.WriteLine("\t3. Ispis podataka po nazivu grada!");
+    Console.WriteLine("\t0. Kraj aplikacije!");
+    option = Console.ReadLine();
+    Console.WriteLine("----------------------------------------------------------------");
+
+    switch (option)
+    { 
+        case "1": 
+        {
+            Console.WriteLine("Unesite broj meseca koji želite (1-12) :");
+            var input = Console.ReadLine();
+            var valid = int.TryParse(input, out var monthId);
+
+            if (!valid)
+            {
+                Console.WriteLine("Greška! Morate uneti broj između 1 i 12!");
+                break;
+            }
+
+                reader.SearchByMonth(monthId).ForEach(r =>
+            {
+                Console.WriteLine(r);
+            });
+
+            break;
+        }
+        case "2":
+        {
+                Console.WriteLine("Unesite ID korisnika koji želite :");
+                var input = Console.ReadLine();
+                var valid = int.TryParse(input, out var userId);
+                if (!valid) 
+                {
+                    Console.WriteLine("Greška! Morate uneti validan ID korisnika!");
+                    break;
+                }
+
+
+                reader.SearchByUser(userId).ForEach(r =>
+                {
+                    Console.WriteLine(r);
+                });
+                break;
+        }
+        case "3":
+        {
+            Console.WriteLine("Unesite naziv grada koji želite :");
+            var input = Console.ReadLine();
+
+            reader.SearchCityName(input).ForEach(r =>
+            {
+                Console.WriteLine(r);
+            });
+
+            break;
+        }
+        default:
+        {
+            Console.WriteLine("Pogrešna opcija! Morate uneti 1, 2 ili 3 kao opciju da bi dobili ispis!");
+            break;
+        }
+    }
+}
---
+++ Implementacija/Reader/Reader.cs
@@ -0,0 +1,40 @@
+﻿using CacheMemory.Structures.Interfaces;
+using CacheMemory.Structures.Payload;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheMemory.Reader
+{
+    public class Reader
+    {
+        private readonly IHistorical hist;
+
+        public Reader(IHistorical historical)
+        {
+            hist = historical;
+        }
+
+        public List<SpentEnergyRecord> SearchByMonth(int month)
+        {
+            return hist.GetRecordsByMonth(month);
+        }
+
+        public List<SpentEnergyRecord> SearchByUser(int userId)
+        {
+            return hist.GetRecordsByUser(userId);
+        }
+
+        public List<SpentEnergyMeter> SearchCityName(string city)
+        {
+            return hist.GetMetersByCityName(city);
+        }
+
+        public void SaveRecords(List<SpentEnergyDto> newRecords)
+        {
+            hist.SaveNewRecords(newRecords);
+        }
+    }
+}
