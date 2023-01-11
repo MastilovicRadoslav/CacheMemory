6a53b09a46775cc933925f52ac9e1290c2618c0b
MastilovicRadoslav <radoslav930@gmail.com>
2023-01-11 16:38:34 +0100

    Klasa Historical

---
+++ Implementacija/Historical/Payload/SpentEnergyDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheMemory.Structures.Payload
+{
+    public record SpentEnergyDto
+    {
+        #region Polja
+        public DateTime Timestamp { get; set; }
+        public int UserId { get; set; }
+        public double SpentEnergy { get; set; }
+        #endregion
+    }
+}
